feccff1fdb457dfc8872ae77634047a066ad9df8
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-01-20 15:56:04 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# MultiRewardDistribution
+
+[![Tests](https://github.com/Wonderland-Money/multi-reward-distribution/actions/workflows/tests.yml/badge.svg)](https://github.com/Wonderland-Money/multi-reward-distribution/actions/workflows/tests.yml)
+
+This repository contains the MultiRewardDistribution.
+
+## Testing
+
+```sh
+$ npm install
+```
+
+Compile hardhat components:
+
+```sh
+$ npm run compile
+```
+
+Run tests:
+
+```sh
+$ npm run test
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,52 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
+  solidity: "0.7.5",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 600
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 200000
+  },
+  typechain: {
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false,
+    externalArtifacts: [
+      'node_modules/@uniswap/v3-core/artifacts/contracts/*/*.json',
+      'node_modules/@uniswap/v3-periphery/artifacts/contracts/*/*.json'
+    ]
+  }
+};
